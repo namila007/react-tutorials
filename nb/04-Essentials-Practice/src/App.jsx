7eabd6f3f64a1409471aea1fde82b4997734e521
Namila Bandara <namila007@gmail.com>
2025-01-08 23:00:07 +0530

    04-Essentials-Practice      - base user inputs done

--- nb/04-Essentials-Practice/src/App.jsx
+++ nb/04-Essentials-Practice/src/App.jsx
@@ -1,5 +1,15 @@
+import { useState } from "react";
+import { UserInputGroup } from "./component/UserInputGroup.jsx";
+import { Header } from "./component/Header.jsx";
+
 function App() {
-  return <h1>React Investment Calculator</h1>;
+  const [value, setValue] = useState({});
+  return (
+    <>
+      <Header />
+      <UserInputGroup stateValue={value} setstateValue={setValue} />
+    </>
+  );
 }
 
 export default App;
---
+++ nb/04-Essentials-Practice/src/component/Header.jsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <header id="header">
+      <img src="/investment-calculator-logo.png" alt="logo" />
+      <h1>React Investment Calculator</h1>
+    </header>
+  );
+}
