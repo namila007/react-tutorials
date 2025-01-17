52e83bd7f917589e915a3580aa74b7f7f1966df7
Namila Bandara <namila007@gmail.com>
2025-01-18 02:44:03 +0530

    06 react ref portals   - timer challenge

--- 06-Ref-Portals/src/App.jsx
+++ 06-Ref-Portals/src/App.jsx
@@ -1,5 +1,6 @@
 import Player from "./components/Player.jsx";
 import { useState } from "react";
+import { TimerChallenge } from "./components/TimerChallenge.jsx";
 
 function App() {
   const [player, setPlayer] = useState("unknown entity");
@@ -7,7 +8,12 @@ function App() {
   return (
     <>
       <Player player={player} setPlayer={setPlayer} />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={1} />
+        <TimerChallenge title="Not Easy" targetTime={5} />
+        <TimerChallenge title="Getting Tough" targetTime={10} />
+        <TimerChallenge title="Pros Only" targetTime={15} />
+      </div>
     </>
   );
 }
