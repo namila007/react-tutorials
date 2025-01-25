83319657305b1f512ee0bc624472be0b976acce8
Namila Bandara <namila007@gmail.com>
2025-01-26 04:21:22 +0530

    07-demo project   - working prototype

--- 07-demo-project-2/src/component/TaskList.jsx
+++ 07-demo-project-2/src/component/TaskList.jsx
@@ -1,7 +1,27 @@
 import Task from "./Task";
+import React from "react";
 
 export default function TaskList({ taskList, setTaskList, projectId }) {
   console.log("TaskList", taskList);
+  // const inputRef = React.useRef(null);  // causes second update
+  const [inputValue, setInputValue] = React.useState("");
+
+  function addTask(e) {
+    e.preventDefault();
+    if (inputValue.trim() === "") return;
+    setTaskList((prevState) => {
+      return prevState.map((project, index) => {
+        if (index === projectId) {
+          return {
+            ...project,
+            tasks: [...project.tasks, inputValue],
+          };
+        }
+        return project;
+      });
+    });
+    setInputValue("");
+  }
 
   function deleteTask(id) {
     console.log("Clear task", id);
@@ -21,7 +41,20 @@ export default function TaskList({ taskList, setTaskList, projectId }) {
   return (
     <>
       <h2>Tasks</h2>
-      <table>
+      <form onSubmit={addTask} className="flex gap-4 mb-6">
+        <input
+          className="flex-1 p-2 border rounded"
+          type="text"
+          // ref={inputRef}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button type="submit" className="text-blue-600 hover:text-blue-800">
+          Add Task
+        </button>
+      </form>
+
+      <table className="flex flex-grow">
         <tbody>
           {taskList.map((task, index) => (
             <tr
