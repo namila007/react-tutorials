30573848803fa69032805415841a1ef65063d138
Namila Bandara <namila007@gmail.com>
2025-02-15 02:54:32 +0530

    08-state-mannagement   - use reducer 09-useEffect   - init

--- 08-Advanced-State-Management/src/store/ShoppingCartContext.jsx
+++ 08-Advanced-State-Management/src/store/ShoppingCartContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer, useState } from "react";
 import { DUMMY_PRODUCTS } from "../dummy-products.js";
 
 const CartContext = createContext({
@@ -6,17 +6,14 @@ const CartContext = createContext({
   addItem: (id) => {},
   updateCartItem: (a, b) => {},
 });
-const CartContextProvider = (props) => {
-  const [shoppingCart, setShoppingCart] = useState({
-    items: [],
-  });
 
-  function handleAddItemToCart(id) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
+function reducer(state, action) {
+  switch (action.type) {
+    case "ADD_ITEM": {
+      const updatedItems = [...state.items];
 
       const existingCartItemIndex = updatedItems.findIndex(
-        (cartItem) => cartItem.id === id,
+        (cartItem) => cartItem.id === action.payload,
       );
       const existingCartItem = updatedItems[existingCartItemIndex];
 
@@ -26,33 +23,32 @@ const CartContextProvider = (props) => {
           quantity: existingCartItem.quantity + 1,
         };
       } else {
-        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+        const product = DUMMY_PRODUCTS.find(
+          (product) => product.id === action.payload,
+        );
         updatedItems.push({
-          id: id,
+          id: action.payload,
           name: product.title,
           price: product.price,
           quantity: 1,
         });
       }
-
       return {
+        ...state,
         items: updatedItems,
       };
-    });
-  }
-
-  function handleUpdateCartItemQuantity(productId, amount) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
+    }
+    case "UPDATE_ITEM": {
+      const updatedItems = [...state.items];
       const updatedItemIndex = updatedItems.findIndex(
-        (item) => item.id === productId,
+        (item) => item.id === action.payload.productId,
       );
 
       const updatedItem = {
         ...updatedItems[updatedItemIndex],
       };
 
-      updatedItem.quantity += amount;
+      updatedItem.quantity += action.payload.amount;
 
       if (updatedItem.quantity <= 0) {
         updatedItems.splice(updatedItemIndex, 1);
@@ -61,13 +57,84 @@ const CartContextProvider = (props) => {
       }
 
       return {
+        ...state,
         items: updatedItems,
       };
+    }
+  }
+
+  return state;
+}
+
+const CartContextProvider = (props) => {
+  const [shoppingCartState, shoppingCartDispatch] = useReducer(reducer, {
+    items: [],
+  });
+  const [shoppingCart, setShoppingCart] = useState();
+
+  function handleAddItemToCart(id) {
+    shoppingCartDispatch({ type: "ADD_ITEM", payload: id });
+    //
+    // setShoppingCart((prevShoppingCart) => {
+    //   const updatedItems = [...prevShoppingCart.items];
+    //
+    //   const existingCartItemIndex = updatedItems.findIndex(
+    //     (cartItem) => cartItem.id === id,
+    //   );
+    //   const existingCartItem = updatedItems[existingCartItemIndex];
+    //
+    //   if (existingCartItem) {
+    //     updatedItems[existingCartItemIndex] = {
+    //       ...existingCartItem,
+    //       quantity: existingCartItem.quantity + 1,
+    //     };
+    //   } else {
+    //     const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+    //     updatedItems.push({
+    //       id: id,
+    //       name: product.title,
+    //       price: product.price,
+    //       quantity: 1,
+    //     });
+    //   }
+    //
+    //   return {
+    //     items: updatedItems,
+    //   };
+    // });
+  }
+
+  function handleUpdateCartItemQuantity(productId, amount) {
+    shoppingCartDispatch({
+      type: "UPDATE_ITEM",
+      payload: { productId, amount },
     });
+    // setShoppingCart((prevShoppingCart) => {
+    //   const updatedItems = [...prevShoppingCart.items];
+    //   const updatedItemIndex = updatedItems.findIndex(
+    //     (item) => item.id === productId,
+    //   );
+    //
+    //   const updatedItem = {
+    //     ...updatedItems[updatedItemIndex],
+    //   };
+    //
+    //   updatedItem.quantity += amount;
+    //
+    //   if (updatedItem.quantity <= 0) {
+    //     updatedItems.splice(updatedItemIndex, 1);
+    //   } else {
+    //     updatedItems[updatedItemIndex] = updatedItem;
+    //   }
+    //
+    //   return {
+    //     items: updatedItems,
+    //   };
+    // });
   }
 
   const ctxValue = {
-    items: shoppingCart.items,
+    items: shoppingCartState.items,
     addItem: handleAddItemToCart,
     updateCartItem: handleUpdateCartItemQuantity,
   };
