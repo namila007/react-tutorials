95d15ddac273a88e603457cd5c53effe8b423f01
Namila Bandara <namila007@gmail.com>
2025-02-16 03:18:06 +0530

    07-Context-Providers   - useeffect

--- 09-Side-Effects-useEffect/src/App.jsx
+++ 09-Side-Effects-useEffect/src/App.jsx
@@ -1,23 +1,43 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import Places from "./components/Places.jsx";
 import { AVAILABLE_PLACES } from "./data.js";
 import Modal from "./components/Modal.jsx";
 import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
 import logoImg from "./assets/logo.png";
+import { sortPlacesByDistance } from "./loc.js";
 
 function App() {
-  const modal = useRef();
+  const jsonPicked = JSON.parse(localStorage.getItem("pickedPlaces")) || [];
+  const picked = jsonPicked.map((id) =>
+    AVAILABLE_PLACES.find((place) => place.id === id),
+  );
+  console.log(picked);
+  // const modal = useRef();
   const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(picked);
+  const [places, setPlaces] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position.coords.latitude, position.coords.longitude);
+      const sortedPlace = sortPlacesByDistance(
+        AVAILABLE_PLACES,
+        position.coords.latitude,
+        position.coords.longitude,
+      );
+      setPlaces(sortedPlace);
+    });
+  }, []);
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    setIsModalOpen(true);
+    console.log("start remove place", id, isModalOpen);
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    setIsModalOpen(false);
   }
 
   function handleSelectPlace(id) {
@@ -25,21 +45,36 @@ function App() {
       if (prevPickedPlaces.some((place) => place.id === id)) {
         return prevPickedPlaces;
       }
-      const place = AVAILABLE_PLACES.find((place) => place.id === id);
+      const place = places.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+    const storedPlaces = JSON.parse(localStorage.getItem("pickedPlaces")) || [];
+    console.log(storedPlaces, storedPlaces.indexOf(id));
+    if (storedPlaces.indexOf(id) === -1) {
+      console.log("place already picked", id, storedPlaces);
+      localStorage.setItem(
+        "pickedPlaces",
+        JSON.stringify([id, ...storedPlaces]),
+      );
+    }
   }
 
   function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current),
     );
-    modal.current.close();
+    setIsModalOpen(false);
+    const storedPlaces = JSON.parse(localStorage.getItem("pickedPlaces")) || [];
+    const pickedPlaces = storedPlaces.filter(
+      (id) => id !== selectedPlace.current,
+    );
+    localStorage.setItem("pickedPlaces", JSON.stringify(pickedPlaces));
   }
 
   return (
     <>
-      <Modal ref={modal}>
+      {console.log("App rendered")}
+      <Modal open={isModalOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
@@ -63,7 +98,7 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={places}
           onSelectPlace={handleSelectPlace}
         />
       </main>
